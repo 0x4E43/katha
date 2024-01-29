8b7b341e7e24fc844ddd9ffe89499636b4894db4
0x4E43 <nimaic.dev@gmail.com>
2024-01-29 23:11:02 +0530

    Potential error detected in custom logger, printing wrong line number

--- main.go
+++ main.go
@@ -37,18 +37,23 @@ func init() {
 func main() {
 	log.Println(utils.INFO("Hello World! This is katha📝"))
 	// Start file server for serving files
+	// file handler kept as it is might not work if uses mux, will see later
+	// TODO: handle Image later
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
 	// Render the HTML file
 
-	http.HandleFunc("/api/options", optionHandler)
-	http.HandleFunc("/search", searchHandler)
-	http.HandleFunc("/", indexHandler)
-	err := http.ListenAndServe(":8080", nil)
+	// TODO: rewrite server with http requests multiplexere
+	mux := http.NewServeMux() // will be possible to route efficiently wilth server mux
+
+	mux.HandleFunc("/", indexHandler)
+
+	mux.HandleFunc("/api/options", optionHandler)
+	mux.HandleFunc("/search", searchHandler)
+	// http.HandleFunc("/", indexHandler)
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Panic(utils.ERROR(err.Error()))
 	}
-
-	// TODO: rewrite server with http requests multiplexere
 
 }
 
