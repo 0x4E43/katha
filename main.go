4625e59d241c0b6a83760e2fb7909c4e906fbd5a
0x4E43 <nimaic.dev@gmail.com>
2024-01-31 21:41:48 +0530

    Added File Server to serve images

--- main.go
+++ main.go
@@ -12,47 +12,44 @@ import (
 
 // Struct for odia english json data
 type EnOdStruct struct {
-	EN_key string // key will be english word
-	OD_val string // value will be meaning of the englisg word in odia lipi
+	EN_key string // key will be the English word
+	OD_val string // value will be the meaning of the English word in Odia lipi
 }
 
 func main() {
 	fmt.Println("Hello World!")
-	//Read the given input Json file and store it in cache
-	//IMPROVEMENT: do not reload the the file on each call
-	data, err := os.ReadFile("En-Od-v3.json")
 
+	// Read the given input JSON file and store it in cache
+	// IMPROVEMENT: do not reload the file on each call
+	data, err := os.ReadFile("En-Od-v3.json")
 	if err != nil {
 		panic(err)
 	}
-	//ummarshal the data to json format using Odia using EnOdStruct
-	// var jsonData EnOdStruct
 
+	// Unmarshal the data to JSON format using EnOdStruct
 	var mapData = make(map[string]string)
 	err = json.Unmarshal(data, &mapData)
 	if err != nil {
 		panic(err)
 	}
+
 	startTime := time.Now().UnixMilli()
 	fmt.Println("Start Time: ", startTime)
 	fmt.Println(mapData["elephant"])
 	fmt.Println("End Time: ", (time.Now().UnixMilli() - startTime))
 
-	//render the HTML File
+	// Start file server for serving files
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
+	// Render the HTML file
 	http.HandleFunc("/", indexHandler)
 	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
 	log.Println("Server Running at : ", 8080)
-
 }
 
 func indexHandler(w http.ResponseWriter, req *http.Request) {
 	templ := template.Must(template.ParseFiles("htmx/index.html"))
 	templ.Execute(w, nil)
-}
-
-func test() string {
-	return "nimai"
 }
